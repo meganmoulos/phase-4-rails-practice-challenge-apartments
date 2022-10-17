850d2dc5bf46b3a88f0d599ff4954e463ca348d5
Megan <meganmoulos@gmail.com>
2022-10-17 15:27:39 -0700

    apartment controller complete

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,8 +1,38 @@
 class ApartmentsController < ApplicationController
 
+    before_action :find_apartment, only: [:show, :update, :destroy] 
+
     def index
-        apartments = Apartment.all
-        render json: apartments
+        render json: Apartment.all
+    end
+
+    def show
+        render json: @apartment
+    end
+
+    def create
+        apartment = Apartment.create(apartment_params)
+        render json: apartment, status: :created
+    end
+
+    def update
+        @apartment.update(apartment_params)
+        render json: @apartment, status: :accepted
+    end
+
+    def destroy
+        @apartment.destroy
+        head :no_content
+    end
+
+    private 
+
+    def find_apartment
+        @apartment = Apartment.find(params[:id])
+    end
+
+    def apartment_params
+        params.permit(:number)
     end
 
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,7 +1,23 @@
 class TenantsController < ApplicationController
 
     def index
-        tenants = Tenant.all
-        render json: tenants
+        render json: Tenant.all
     end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+    
 end
